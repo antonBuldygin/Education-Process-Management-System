7824897e2a2c3fe73b1503e27b4a4436d79113e6
Alex <lipriconny@gmail.com>
2022-03-02 03:18:59 +0700

    project stucture(delete unused imports)

--- backend/src/main/java/ru/edu/project/backend/app/EPMSBackendApplication.java
+++ backend/src/main/java/ru/edu/project/backend/app/EPMSBackendApplication.java
@@ -2,15 +2,7 @@ package ru.edu.project.backend.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.Scope;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-
-import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 @SpringBootApplication
 @ComponentScan({"ru.edu.project.backend"})
--- frontend/src/main/java/ru/edu/project/app/RemoteServiceConfig.java
+++ frontend/src/main/java/ru/edu/project/app/RemoteServiceConfig.java
@@ -1,11 +1,8 @@
 package ru.edu.project.app;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import ru.edu.project.backend.RestServiceInvocationHandler;
-import ru.edu.project.backend.api.solutions.SolutionService;
-import ru.edu.project.backend.api.tasks.TaskService;
 
 import java.lang.reflect.Proxy;
 
