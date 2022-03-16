a39b7386673e8ca0d19fabc18eaf142c841eb14f
Alex <lipriconny@gmail.com>
2022-03-16 21:46:26 +0700

    Added group on student creation form. Templates fixing.

--- frontend/src/main/java/ru/edu/project/app/EPMSFrontendApplication.java
+++ frontend/src/main/java/ru/edu/project/app/EPMSFrontendApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 @ComponentScan({"ru.edu.project"})
 public class EPMSFrontendApplication {
